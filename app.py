eb97fd127a8fc35e6beb0d383136a748709aa39d
Cody Beznec <cdbeznec@outlook.com>
2025-04-01 01:19:42 -0400

    k

--- app.py
+++ app.py
@@ -38,7 +38,13 @@ app = Flask(__name__)
 
 bcrypt = Bcrypt(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+uri = os.getenv('DATABASE_URL')
+
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
